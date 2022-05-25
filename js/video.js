4d675188724ef6fee37f265ba7dd56feef2487c1
Aechelon-Online <info@aechelon.online>
2022-05-25 12:32:08 -0500

    adding internal links in reviews

--- js/video.js
+++ js/video.js
@@ -1,14 +1,3 @@
-const videoConsole = document.querySelector('.video-wrapper')
-const video = videoConsole.querySelectorAll('.video')
-const playButton = document.querySelectorAll('.play-button')
-const volume = videoConsole.querySelectorAll('.volume')
-const currentTimeElement = videoConsole.querySelectorAll('.current')
-const durationTimeElement = videoConsole.querySelectorAll('.duration')
-const progress = videoConsole.querySelectorAll('.video-progress')
-const progressBar = videoConsole.querySelectorAll('.video-progress-filled')
-
-
-
 
 
 const videos = {
@@ -32,27 +21,3 @@ videoPlayer += `
 document.querySelector('.video-wrapper').innerHTML = videoPlayer
 
 
-// document.querySelectorAll('.play-button').addEventListener('click', function(e){
-//     if (video.paused) {
-//         video.play()
-//         e.target.textContent = '❚❚'
-//     } else {
-//         video.pause()
-//         e.target.textContent = '▶'
-//     }
-    
-// })
-
-{/* <div class="player-controls">
-            <div class="video-progress">
-                <div class="video-progress-filled"></div>
-            </div>
-
-            <button class="play-button" title="play">▶</button>
-
-            <input type="range" class="volume" min="0" max="1" step="0.01" value="1" />
-    
-            <div class="time">
-                <span class="current">0:00</span> / <span class="duration">0:00</span>
-            </div>
-        </div> */}
